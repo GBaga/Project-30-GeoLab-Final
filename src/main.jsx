be37b39a19dc8d11db7b828af8d86ca204547b2c
Goga <g.bagauri@yahoo.com>
2025-02-26 14:13:19 +0400

    react-query added
    react query is used only in main page, but no other places.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import "./index.css";
 import Geocode from "./components/Geocode.jsx";
 import CurrentWeather from "./pages/CurrentWeather.jsx";
@@ -9,18 +11,22 @@ import History from "./pages/History.jsx";
 import DailyTemp from "./pages/DailyTemp.jsx";
 import Forecast from "./pages/Forecast.jsx";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<CurrentWeather />} />
-          <Route path="/geocode" element={<Geocode />} />
-          <Route path="/forecast" element={<Forecast />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/detailed-history" element={<DailyTemp />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<CurrentWeather />} />
+            <Route path="/geocode" element={<Geocode />} />
+            <Route path="/forecast" element={<Forecast />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/detailed-history" element={<DailyTemp />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
