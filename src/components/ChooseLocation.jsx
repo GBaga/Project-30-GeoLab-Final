11a51260a4574b2154d6d4a3a096cd297a8a7bd3
Goga <g.bagauri@yahoo.com>
2025-03-01 21:36:03 +0400

    added city   "Chkhorotsku"

--- src/components/ChooseLocation.jsx
+++ src/components/ChooseLocation.jsx
@@ -85,6 +85,7 @@ const ChooseLocation = () => {
         <option value="Ambrolauri">{t("Ambrolauri")}</option>
         <option value="Kobuleti">{t("Kobuleti")}</option>
         <option value="Zestafoni">{t("Zestafoni")}</option>
+        <option value="Chkhorotsku">{t("Chkhorotsku")}</option>
       </select>
 
       <div className="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-fit">
